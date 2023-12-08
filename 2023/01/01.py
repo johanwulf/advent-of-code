eeed4dcc09cbe946dc28897406b273862d7429bd
johanwulf <johanwulf98@gmail.com>
2023-12-08 17:04:35 +0100

    01: cleanup

--- 2023/01/01.py
+++ 2023/01/01.py
@@ -1,12 +1,15 @@
+
+input = open("input", "r")
+ll = [x for x in input.read().strip().split("\n")]
+
+print(ll)
 def part_a():
     arr = []
-    with open("input", "r") as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            line = "".join(j for j in lines[i] if j.isdigit())
-            numb = str(line[0]) + str(line[-1])
-            arr.append(int(numb))
-    print(sum(arr))
+    for l in ll:
+        line = "".join(j for j in l if j.isdigit())
+        numb = str(line[0]) + str(line[-1])
+        arr.append(int(numb))
+    print("Part one:", sum(arr))
 
 def part_b():
     arr = []
@@ -22,16 +25,14 @@ def part_b():
         "nine": "n9ne"
     }
 
-    with open("input", "r") as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            line = lines[i]
-            for k, v in num.items():
-                line = line.replace(k, v)
-            line = "".join(j for j in line if j.isdigit())
-            numb = str(line[0]) + str(line[-1])
-            arr.append(int(numb))
-    print(sum(arr))
+    for l in ll:
+        for k, v in num.items():
+            l = l.replace(k, v)
+        line = "".join(j for j in l if j.isdigit())
+        numb = str(line[0]) + str(line[-1])
+        arr.append(int(numb))
+
+    print("Part two:", sum(arr))
 
 part_a()
 part_b()
