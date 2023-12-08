4b5d05311b646ec9189fa73487258d543e5869b1
johanwulf <johanwulf98@gmail.com>
2023-12-08 17:32:19 +0100

    02: cleanup

--- 2023/02/02.py
+++ 2023/02/02.py
@@ -1,62 +1,44 @@
-import re
-
 input = open("input", "r")
-
 ll = [x for x in input.read().strip().split("\n")]
 
 def part_a():
-    ret = 0
-    flag = True
+    ret = []
     for l in ll:
-        game = l.split(" ")[1].strip(":")
-        subset = l.split(":")[1].strip().split(";")
+        game_number = l.split()[1].strip(":")
+        game = l.split(":")[1].strip().split(";")
         flag = True 
     
-        for i in range(len(subset)):
-            red = 12
-            green = 13
-            blue = 14
-            nam = subset[i].split(",")
-            for i in range(len(nam)):
-                val = re.search(r'\d+', nam[i])
-                sum = 0
-                if val:
-                    sum = int(val.group())
-                if "red" in nam[i]:
-                    red -= sum 
-                elif "green" in nam[i]:
-                    green -= sum 
-                elif "blue" in nam[i]:
-                    blue -= sum
-            if red < 0 or green < 0 or blue < 0:
-                flag = False
+        for g in game:
+            cubes = {"red": 12, "green": 13, "blue": 14}
+            subset = g.strip().split(",")
+            for i in range(len(subset)):
+                val, color = subset[i].strip().split(" ")
+                cubes[color] -= int(val)
+
+            for _, value in cubes.items():
+                if value < 0:
+                    flag = False
+                    break
 
         if flag:
-            ret += int(game) 
+            ret.append(int(game_number))
+
     print(ret)
+    print("Part one", sum(ret))
 
 def part_b():
     ret = 0
     for l in ll:
-        subset = l.split(":")[1].strip().split(";")
-        red = 0
-        green = 0
-        blue = 0 
-        for i in range(len(subset)):
-            nam = subset[i].split(",")
-            for i in range(len(nam)):
-                val = re.search(r'\d+', nam[i])
-                sum = 0
-                if val:
-                    sum = int(val.group())
-                if "red" in nam[i] and sum > red:
-                    red = sum
-                elif "green" in nam[i] and sum > green:
-                    green = sum
-                elif "blue" in nam[i] and sum > blue:
-                    blue = sum
-        ret += red * blue * green
-    print(ret)
+        game = l.split(":")[1].strip().split(";")
+        cubes = {"red": 0, "green": 0, "blue": 0}
+        for g in game:
+            subset = g.strip().split(",")
+            for s in subset: 
+                val, color = s.strip().split(" ")
+                cubes[color] = max(cubes[color], int(val))
+
+        ret += cubes["red"] * cubes["green"] * cubes["blue"]
+    print("Part two", ret)
 
 part_a()
 part_b()
